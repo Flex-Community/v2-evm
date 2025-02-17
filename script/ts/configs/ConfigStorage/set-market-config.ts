292ac51d3c5870ea26689f40bdc325dce5aca4bb
FlexDev <flexdev@flex.community>
2025-02-17 14:24:10 -0400

    update scripts

--- script/ts/configs/ConfigStorage/set-market-config.ts
+++ script/ts/configs/ConfigStorage/set-market-config.ts
@@ -36,8 +36,8 @@ async function main(chainId: number) {
     {
       marketIndex: 0,
       assetId: ethers.utils.formatBytes32String("ETH"),
-      maxLongPositionSize: ethers.utils.parseUnits(String(20_000_000), 30),
-      maxShortPositionSize: ethers.utils.parseUnits(String(20_000_000), 30),
+      maxLongPositionSize: ethers.utils.parseUnits(String(1_000_000), 30),
+      maxShortPositionSize: ethers.utils.parseUnits(String(1_000_000), 30),
       increasePositionFeeRateBPS: 2, // 0.02%
       decreasePositionFeeRateBPS: 2, // 0.02%
       initialMarginFractionBPS: 100, // IMF = 1%, Max leverage = 100
@@ -47,7 +47,7 @@ async function main(chainId: number) {
       allowIncreasePosition: true,
       active: true,
       fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("2000000000", 30), // 2000 M
+        maxSkewScaleUSD: ethers.utils.parseUnits(String(2_000_000_000), 30), // 2000 M
         maxFundingRate: ethers.utils.parseUnits("8", 18), // 900% per day
       },
       isAdaptiveFeeEnabled: false,
@@ -55,8 +55,8 @@ async function main(chainId: number) {
     {
       marketIndex: 1,
       assetId: ethers.utils.formatBytes32String("BTC"),
-      maxLongPositionSize: ethers.utils.parseUnits(String(20_000_000), 30),
-      maxShortPositionSize: ethers.utils.parseUnits(String(20_000_000), 30),
+      maxLongPositionSize: ethers.utils.parseUnits(String(1_000_000), 30),
+      maxShortPositionSize: ethers.utils.parseUnits(String(1_000_000), 30),
       increasePositionFeeRateBPS: 2, // 0.02%
       decreasePositionFeeRateBPS: 2, // 0.02%
       initialMarginFractionBPS: 100, // IMF = 1%, Max leverage = 100
@@ -66,7 +66,7 @@ async function main(chainId: number) {
       allowIncreasePosition: true,
       active: true,
       fundingRate: {
-        maxSkewScaleUSD: ethers.utils.parseUnits("3000000000", 30), // 3000 M
+        maxSkewScaleUSD: ethers.utils.parseUnits(String(3_000_000_000), 30), // 3000 M
         maxFundingRate: ethers.utils.parseUnits("8", 18), // 900% per day
       },
       isAdaptiveFeeEnabled: false,
