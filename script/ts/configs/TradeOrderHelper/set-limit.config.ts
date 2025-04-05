f7daf1bd1f18150547d00b7d4c252ff841ce75d3
FlexDev <flexdev@flex.community>
2025-04-05 15:25:41 -0300

    extrude config to a separate file

---
+++ script/ts/configs/TradeOrderHelper/set-limit.config.ts
@@ -0,0 +1,5 @@
+export const SET_LIMITS_CONFIG  = [
+  { marketIndex: 0, positionSizeLimit: 250_000, tradeSizeLimit: 250_000 },
+  { marketIndex: 1, positionSizeLimit: 250_000, tradeSizeLimit: 250_000 },
+  { marketIndex: 2, positionSizeLimit: 100_000, tradeSizeLimit: 100_000 },
+];
