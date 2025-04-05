50a900d05e2df5864af9de91a2dd2c42af6231b9
FlexDev <flexdev@flex.community>
2025-04-05 15:51:51 -0300

    extrude config to a separate file for OracleMiddleware/set-asset-price-configs.ts

---
+++ script/ts/configs/OracleMiddleware/set-asset-price-configs.cfg.ts
@@ -0,0 +1,33 @@
+import { ethers } from "ethers";
+import { loadConfig } from "../../utils/config";
+
+export function getConfig_SetAssetPriceConfigs(chainId:number) {
+  const config = loadConfig(chainId);
+  return [
+    {
+      assetId: ethers.utils.formatBytes32String("ETH"),
+      confidenceThreshold: 0,
+      trustPriceAge: 60 * 5, // 5 minutes
+      adapter: config.oracles.pythAdapter,
+    },
+    {
+      assetId: ethers.utils.formatBytes32String("BTC"),
+      confidenceThreshold: 0,
+      trustPriceAge: 60 * 5, // 5 minutes
+      adapter: config.oracles.pythAdapter,
+    },
+    {
+      assetId: ethers.utils.formatBytes32String("USDC"),
+      confidenceThreshold: 0,
+      trustPriceAge: 60 * 5, // 5 minutes
+      adapter: config.oracles.pythAdapter,
+    },
+    {
+      assetId: ethers.utils.formatBytes32String("SOL"),
+      confidenceThreshold: 0,
+      trustPriceAge: 60 * 5, // 5 minutes
+      adapter: config.oracles.pythAdapter,
+    },
+  ];
+
+}
