7f1105eea741565dc0b0d965dea5089cbcd8b54b
FlexDev <flexdev@flex.community>
2025-04-08 13:55:53 +0000

    update script: LimitTradeHandlerupdate minExecutionFee

--- script/ts/configs/LimitTradeHandler/set-min-execution-fee.ts
+++ script/ts/configs/LimitTradeHandler/set-min-execution-fee.ts
@@ -1,27 +1,42 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
 import { ethers } from "hardhat";
-import { EvmPriceServiceConnection } from "@pythnetwork/pyth-evm-js";
 import {
-  IPyth__factory,
   LimitTradeHandler__factory,
-  MockPyth__factory,
-  PythAdapter__factory,
 } from "../../../../typechain";
 import { getConfig } from "../../utils/config";
+import readlineSync from 'readline-sync';
 
 const config = getConfig();
-const BigNumber = ethers.BigNumber;
 const parseUnits = ethers.utils.parseUnits;
 
 async function main() {
   const deployer = (await ethers.getSigners())[0];
 
-  console.log("> LimitTradeHandler: setMinExecutionFee...");
+  console.log("> LimitTradeHandler: Checking current minExecutionFee...");
   const limitTradeHandler = LimitTradeHandler__factory.connect(config.handlers.limitTrade, deployer);
-  await (await limitTradeHandler.setMinExecutionFee(0)).wait();
+  
+  const currentFee = await limitTradeHandler.minExecutionFee();
+  const newFee = parseUnits("0.00003", 18); // Convert to wei
+  
+  console.log(`Current minExecutionFee: ${ethers.utils.formatEther(currentFee)} ETH (${currentFee.toString()} wei)`);
+  console.log(`New minExecutionFee: ${ethers.utils.formatEther(newFee)} ETH (${newFee.toString()} wei)`);
+  
+  if (currentFee.eq(newFee)) {
+    console.log("🟢 minExecutionFee is already set to the desired value");
+    return;
+  }
+
+  const answer = readlineSync.question(`Do you want to change minExecutionFee from ${ethers.utils.formatEther(currentFee)} ETH to ${ethers.utils.formatEther(newFee)} ETH? (y/n): `);
+
+  if (answer.toLowerCase() !== 'y') {
+    console.log("Operation cancelled by user");
+    return;
+  }
+
+  console.log("> LimitTradeHandler: Setting minExecutionFee...");
+  await (await limitTradeHandler.setMinExecutionFee(newFee)).wait();
   console.log("> LimitTradeHandler: setMinExecutionFee success!");
 }
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
