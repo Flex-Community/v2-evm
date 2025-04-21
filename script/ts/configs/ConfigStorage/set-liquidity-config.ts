0387d77fe5dc029b306d57093ef9869123e54aad
fpcrypto-y <yk@flexdex.finance>
2025-04-21 18:16:46 +0000

    update script: show difference before update

--- script/ts/configs/ConfigStorage/set-liquidity-config.ts
+++ script/ts/configs/ConfigStorage/set-liquidity-config.ts
@@ -1,14 +1,15 @@
+import { BigNumber, ethers } from "ethers";
 import { ConfigStorage__factory } from "../../../../typechain";
 import { loadConfig } from "../../utils/config";
-import { Command } from "commander";
-import { OwnerWrapper } from "../../wrappers/OwnerWrapper";
+import chalk from 'chalk';
 import signers from "../../entities/signers";
 import { passChainArg } from "../../utils/main-fn-wrappers";
+import { OwnerWrapper } from "../../wrappers/OwnerWrapper";
 
 const liquidityConfig = {
   depositFeeRateBPS: 0, // 0%
   withdrawFeeRateBPS: 30, // 0.3%
-  maxHLPUtilizationBPS: 8000, // 80%
+  maxHLPUtilizationBPS: 7000, // 70%
   hlpTotalTokenWeight: 0, // DEFAULT, auto calculated by assetHlpTokenConfigs
   hlpSafetyBufferBPS: 2000, // 20%
   taxFeeRateBPS: 50, // 0.5%
@@ -23,11 +24,60 @@ async function main(chainId: number) {
   const ownerWrapper = new OwnerWrapper(chainId, deployer);
   const configStorage = ConfigStorage__factory.connect(config.storages.config, deployer);
 
-  console.log("[configs/ConfigStorage] Set Liquidity Config...");
-  await ownerWrapper.authExec(
-    configStorage.address,
-    configStorage.interface.encodeFunctionData("setLiquidityConfig", [liquidityConfig])
-  );
+  console.log("[configs/ConfigStorage] Checking Liquidity Config...");
+
+  // Get current liquidity config from contract
+  const existingLiquidityConfig = await configStorage.getLiquidityConfig();
+
+  function compareConfigs(existingConfig: any, newConfig: any): any {
+    const differences: any = [];
+
+    function compareValues(key: string, existingValue: any, newValue: any) {
+      if (existingValue !== newValue) {
+        differences.push({
+          key,
+          existingValue,
+          newValue,
+        });
+      }
+    }
+
+    function compareObjects(existingObj: any, newObj: any, parentKey = '') {
+      for (const key in newObj) {
+        const fullKey = parentKey ? `${parentKey}.${key}` : key;
+        if (typeof newObj[key] === 'object' && newObj[key] !== null) {
+          compareObjects(existingObj[key], newObj[key], fullKey);
+        } else {
+          compareValues(fullKey, existingObj[key], newObj[key]);
+        }
+      }
+    }
+
+    compareObjects(existingConfig, newConfig);
+    return differences;
+  }
+
+  const differences = compareConfigs(existingLiquidityConfig, liquidityConfig);
+
+  if (differences.length > 0) {
+    console.log(chalk.red(`Differences found:`));
+    differences.forEach((diff: any) => {
+      console.log(
+        `${chalk.yellow(diff.key)}: ${chalk.red(diff.existingValue)} -> ${chalk.green(diff.newValue)}`
+      );
+      if (String(diff.existingValue).startsWith('0x')) {
+        console.log(`  ${BigNumber.from(diff.existingValue).toString()} -> ${BigNumber.from(diff.newValue).toString()}`);
+      }
+    });
+
+    console.log("[configs/ConfigStorage] Set Liquidity Config...");
+    const tx = await ownerWrapper.authExec(
+      configStorage.address,
+      configStorage.interface.encodeFunctionData("setLiquidityConfig", [liquidityConfig])
+    );
+  } else {
+    console.log(chalk.green(`No differences found. Skipping update.`));
+  }
 }
 
 passChainArg(main)
