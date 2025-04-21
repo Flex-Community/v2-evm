90c446d3e3359c0dac210f64e0aa5310c6685753
fpcrypto-y <yk@flexdex.finance>
2025-04-21 17:52:31 +0000

    update limits config

--- script/ts/configs/TradeOrderHelper/set-limit.config.ts
+++ script/ts/configs/TradeOrderHelper/set-limit.config.ts
@@ -1,5 +1,5 @@
 export const SET_LIMITS_CONFIG  = [
-  { marketIndex: 0, positionSizeLimit: 250_000, tradeSizeLimit: 250_000 },
-  { marketIndex: 1, positionSizeLimit: 250_000, tradeSizeLimit: 250_000 },
-  { marketIndex: 2, positionSizeLimit: 100_000, tradeSizeLimit: 100_000 },
+  { marketIndex: 0, positionSizeLimit: 300_000, tradeSizeLimit: 300_000 },
+  { marketIndex: 1, positionSizeLimit: 300_000, tradeSizeLimit: 300_000 },
+  { marketIndex: 2, positionSizeLimit: 150_000, tradeSizeLimit: 150_000 },
 ];
