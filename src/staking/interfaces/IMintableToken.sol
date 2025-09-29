2d671a3f91d589e0a335505e1bcbb304835bee87
flexcoder17 <arcpinus@gmail.com>
2025-09-29 12:46:57 -0400

    fix comments

--- src/staking/interfaces/IMintableToken.sol
+++ src/staking/interfaces/IMintableToken.sol
@@ -6,7 +6,7 @@ pragma solidity 0.8.18;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface MintableTokenInterface is IERC20 {
+interface IMintableToken is IERC20 {
   function isMinter(address _minter) external view returns (bool);
 
   function setMinter(address minter, bool allow) external;
